a66d3cc62488f293ed7358f88e73a603f18317c0
Juuso <kortelaine@gmail.com>
2021-10-20 11:16:27 +0300

    First Commit

---
+++ TestProject/src/testPackage/Testi.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package testPackage;
+/**
+ * @author juuso
+ *
+ */
+public class Testi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
